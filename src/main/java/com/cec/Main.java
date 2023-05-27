e5cc7ccd852e62e9efdb39e4636802d8eb57a743
Cecilia Deng <ceciliadeng12@berkeley.edu>
2023-05-27 14:10:21 -0700

    creating beans

--- src/main/java/com/cec/Main.java
+++ src/main/java/com/cec/Main.java
@@ -4,8 +4,12 @@ package com.cec;
 import com.cec.customer.CustomerController;
 import com.cec.customer.CustomerDataAccessService;
 import com.cec.customer.CustomerService;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -21,7 +25,26 @@ import java.util.Objects;
 public class Main {
 
     public static void main(String[] args) {
+        ConfigurableApplicationContext applicationContext =
+            SpringApplication.run(Main.class, args);
+        printBeans(applicationContext);
+    }
 
-        SpringApplication.run(Main.class, args);
+    @Bean("foo")
+    @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
+    public Foo getFoo() {
+        return new Foo("bar");
+    }
+
+    record Foo(String name) {
+
+    }
+
+    private static void printBeans(ConfigurableApplicationContext ctx) {
+        String[] beanDefinitionNames =
+                ctx.getBeanDefinitionNames();
+        for (String beanDef : beanDefinitionNames) {
+            System.out.println(beanDef);
+        }
     }
 }
