ad253607e8c5785a3fef5d761a3fb921183820c9
Cecilia Deng <ceciliadeng12@berkeley.edu>
2023-07-30 03:52:16 -0700

    jdbc curd setup

--- src/main/java/com/cec/customer/Customer.java
+++ src/main/java/com/cec/customer/Customer.java
@@ -43,8 +43,14 @@ public class Customer {
 
     public Customer(){}
 
+    public Customer(Integer id, String name, String email, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
     public Customer(String name, String email, Integer age) {
-//        this.id = id;
         this.name = name;
         this.email = email;
         this.age = age;
